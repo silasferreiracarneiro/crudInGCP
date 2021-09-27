22d652d80075339a94e0d85242d66d6655f61462
Silas Ferreira <silasferreira@MacBook-Pro-de-Silas.local>
2021-09-27 11:29:32 -0300

    adding files

---
+++ src/main/kotlin/br/com/silascarneiro/demoCloud/democloudgcp/DemoCloudGcpApplication.kt
@@ -0,0 +1,11 @@
+package br.com.silascarneiro.demoCloud.democloudgcp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoCloudGcpApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoCloudGcpApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/silascarneiro/demoCloud/democloudgcp/DemoCloudGcpApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.silascarneiro.demoCloud.democloudgcp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoCloudGcpApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
